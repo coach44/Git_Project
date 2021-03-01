a3445b2cbb86ebc26578894834b5c7133500cb59
Koch <beredtrade@gmail.com>
2021-03-01 17:29:17 -0500

    feature

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,9 +5,9 @@ public class Class1 {
 		System.out.println("1 gun saat 11:15");
 		System.out.println(" second commit ");
 		System.out.println("10:11 - ikinci gun");
-                System.out.println("11:11 - github.com");
+        System.out.println("11:11 - github.com");
 		System.out.println("11:21 - comflict is coming or not ");
-
+		System.out.println("10:26 - feature branch ");
 	}
 
 }
